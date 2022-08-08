b3de5af9f93a4bcd69c422cf3746e256e9f11883
Marco Antonio Cabrera Chavez <macabrera@intermexusa.com>
2022-08-08 17:11:18 -0500

    Cambios de POO

---
+++ POOConcepts/POOConcepts/Date.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOConcepts
+{
+    public class Date
+    {
+        private int _year;
+        private int _month;
+        private int _day;
+
+        public Date(int year, int month, int day)
+        {
+            _year = year;   
+            _month = CheckMonth(month); 
+            _day = CheckDay(year, month, day);
+        }
+
+        private int CheckDay(int day, int month, int year)
+        {
+            if(month == 2 && day == 29 && IsSleepYear(year))
+            {
+                return day;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        private bool IsSleepYear(int year)
+        {
+            return year % 400 == 0 
+        }
+
+        private int CheckMonth(int month)
+        {
+            if (month >= 1 && month < 12)
+            {
+                return month;
+            }
+
+            throw new MonthException("Invalid month");
+        }
+
+        public override string ToString()
+        {
+            return $"{_year}/{_month:00}/{_day:00}";
+        }
+    }
+
+}
---
+++ POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,15 @@
+﻿using POOConcepts;
+
+Console.WriteLine("Conceptos de POO!");
+Console.WriteLine("==============");
+try
+{
+    Console.WriteLine(new Date(2022, 12, 7));
+    Console.WriteLine(new Date(1974, 9, 23));
+    Console.WriteLine(new Date(1985, 18, 45));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
+
