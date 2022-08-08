d23b88826e8ae80123fbbdbd44da3f49f21470f6
Marco Antonio Cabrera Chavez <macabrera@intermexusa.com>
2022-08-08 18:56:05 -0500

    Cambios 08/08/2022

--- POOConcepts/POOConcepts/Date.cs
+++ POOConcepts/POOConcepts/Date.cs
@@ -15,40 +15,68 @@ namespace POOConcepts
 
         public Date(int year, int month, int day)
         {
-            _year = year;   
-            _month = CheckMonth(month); 
+            _year = year;
+            _month = CheckMonth(month);
             _day = CheckDay(year, month, day);
         }
 
-        private int CheckDay(int day, int month, int year)
+        private int CheckDay(int year, int month, int day)
         {
-            if(month == 2 && day == 29 && IsSleepYear(year))
+            if (month == 2 && day == 29 && IsLeapYear(year))
             {
                 return day;
             }
 
-            throw new NotImplementedException();
+            int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if (day >= 1 && day <= daysPerMonth[month])
+            {
+                return day;
+            }
+
+            throw new DayException("Invalid day");
         }
 
-        private bool IsSleepYear(int year)
+        private bool IsLeapYear(int year)
         {
-            return year % 400 == 0 
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+            //if (year % 4 == 0)
+            //{
+            //    if (year % 100 == 0)
+            //    {
+            //        if (year % 400 == 0)
+            //        {
+            //            return true;
+            //        }
+            //        else
+            //        {
+            //            return false;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        return true;
+            //    }
+            //}
+            //else
+            //{
+            //    return false;
+            //}
         }
 
         private int CheckMonth(int month)
         {
-            if (month >= 1 && month < 12)
+            if (month >= 1 && month <= 12)
             {
                 return month;
             }
 
-            throw new MonthException("Invalid month");
+            throw new MonthException($"Invalid month");
         }
 
         public override string ToString()
         {
             return $"{_year}/{_month:00}/{_day:00}";
         }
-    }
 
+    }
 }
--- POOConcepts/POOConcepts/Program.cs
+++ POOConcepts/POOConcepts/Program.cs
@@ -4,9 +4,9 @@ Console.WriteLine("Conceptos de POO!");
 Console.WriteLine("==============");
 try
 {
-    Console.WriteLine(new Date(2022, 12, 7));
+    Console.WriteLine(new Date(2024, 29, 2));
     Console.WriteLine(new Date(1974, 9, 23));
-    Console.WriteLine(new Date(1985, 18, 45));
+    Console.WriteLine(new Date(1985, 09, 45));
 }
 catch (Exception error)
 {
